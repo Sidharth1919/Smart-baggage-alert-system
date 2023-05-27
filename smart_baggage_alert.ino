679a094e6ee7527244382d1b6e3e080817d3b75f
Sidharth Ajul <88034533+Sidharth1919@users.noreply.github.com>
2023-05-27 10:52:00 +0530

    Add files via upload

---
+++ smart_baggage_alert.ino
@@ -0,0 +1,111 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "S"; //Enter wifi hotspot name// 
+const char* password = "123456789"; //Enter wifi hotspot password//
+#define BOTtoken "5937349344:AAF7Z1Zw1LBPTqg6WTtI4n6cU2wDVUhrtok" //Paste telegram BOT token//
+#define CHAT_ID "1650783203" //Paste Telegram chat ID//
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+void setup()
+
+{
+
+Serial.begin(115200);
+
+Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); 
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
+
+
+
+delay(100);
+
+Serial.println("Setup done");
+
+}
+
+void loop()
+
+{
+
+  Serial.println("scan start");
+
+
+  int n = WiFi.scanNetworks();
+
+  Serial.println("scan done");
+
+  if (n == 0) 
+  {
+
+    Serial.println("no networks found");
+
+  } 
+  else 
+  {
+
+      Serial.print(n);
+
+      Serial.println(" networks found");
+
+    for (int i = 0; i < n; ++i) {
+
+//delay(10);
+if((WiFi.SSID(i))=="S")
+{
+int count;
+  Serial.print(WiFi.SSID(i));
+  Serial.println(WiFi.RSSI(i));
+  if(WiFi.RSSI(i)<-10)
+  {
+    delay(10000);
+
+    //count=count+1;
+    //if
+    //if(count==10)
+    if(WiFi.RSSI(i)<-20 || WiFi.status() != WL_CONNECTED )
+    {
+
+      Serial.println("low signal");
+    
+      bot.sendMessage(CHAT_ID, "Did you forget your luggage?", "");
+      Serial.println("Message sent");
+      
+
+      
+    }
+    
+  }
+  
+}
+
+
+}
+
+}
+
+Serial.println("");
+\
+
+delay(1000);
+
+}
